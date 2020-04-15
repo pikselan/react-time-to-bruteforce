2cdb041ffcca70f2d0ba53e3c7f700c945657b10
pikselan <faif@pikselan.com>
2020-04-15 23:20:17 +0700

    init react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Time To Bruteforce your Password</h1>
+        <input type="text"></input>
+        <h4> Time : </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
